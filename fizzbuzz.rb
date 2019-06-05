87c8298457fbbe65e20a1e62a9da4d2297a5c785
observant-communicator-1765 <timothyjshen@gmail.com>
2019-06-05 21:05:32 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,4 +1,8 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
 def fizzbuzz (int)
+  if int % 3 == 0
+    puts "Fizz"
+  else
+    puts "nil"
 end
